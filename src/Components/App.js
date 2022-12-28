df0706963d2a9af948ef3449e429fd5e5d2caf78
laptopVinh <xuanvinh2401@gmail.com>
2022-12-28 15:22:36 +0700

    first push

--- src/Components/App.js
+++ src/Components/App.js
@@ -6,37 +6,12 @@ import '../styles/App.css';
 import { Routes, Route, Link } from "react-router-dom";
 import store from "../Redux/store";
 // import { demo } from "../Redux/Actions/Action";
-
 import AssetMove from "./Asset/AssetMove";
 import AssetRepair from "./Asset/AssetRepair";
 // import { json } from "express";
-
 class App extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = { 
-  //     dataAsset:[
-  //       {
-  //         id:'',
-  //         assetname:'Laptop Dell 5520',
-  //         buyday:'',
-  //         price:'20.000.000',
-  //         supplier:'365',
-  //         warranty:'24',
-  //         location:'vp208',
-  //         userid:'vp208',
-  //         note:'New full box',
-  //         demo:"Vinh"
-  //       } 
-  //      ]
-  //    };
-    
-  // }
-       render() {      
-      // const {dataAsset} = this.state
-      // const dataRedux =store.getState(store)
-      // console.log(dataRedux.demo)
-      // store.dispatch(demo)
+         render() {      
+   
         return (   
         <Routes>
           <Route exact  path="/" element={<AssetCategory />} />
